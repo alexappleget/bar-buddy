f6a43683e2e311d2410dcf342490438280b4cb36
Alex Appleget <alexappleget2014@gmail.com>
2024-11-18 16:40:58 -0600

    Feat: added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import LinkCustom from "@/components/LinkCustom";
+
 const Home = () => {
   return (
-    <section className="h-screen flex flex-col items-center justify-center">
-      <h1>Welcome to Bar Buddy!</h1>
+    <section className="h-screen flex flex-col items-center justify-center gap-14">
+      <h1 className="text-xl">Welcome to Bar Buddy!</h1>
+      <LinkCustom href="">Get Started</LinkCustom>
     </section>
   );
 };
+
+export default Home;
