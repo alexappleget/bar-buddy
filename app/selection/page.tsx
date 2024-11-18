2fada63205df36ce663dfad71346433741d4ff71
Alex Appleget <alexappleget2014@gmail.com>
2024-11-18 17:39:31 -0600

    Feat: creating liquors data

--- app/selection/page.tsx
+++ app/selection/page.tsx
@@ -1,3 +1,48 @@
+import { Checkbox } from "@/components/CheckBox/checkbox";
+
+const Liquors = [
+  {
+    id: "vodka",
+    label: "Vodka",
+  },
+  {
+    id: "whiskey",
+    label: "Whiskey",
+  },
+  {
+    id: "rum",
+    label: "Rum",
+  },
+  {
+    id: "tequila",
+    label: "Tequila",
+  },
+  {
+    id: "gin",
+    label: "Gin",
+  },
+  {
+    id: "brandy",
+    label: "Brandy",
+  },
+  {
+    id: "mezcal",
+    label: "Mezcal",
+  },
+  {
+    id: "absinthe",
+    label: "Absinthe",
+  },
+  {
+    id: "schnapps",
+    label: "Schnapps",
+  },
+  {
+    id: "aquavit",
+    label: "Aquavit",
+  },
+];
+
 const Selection = () => {
   return <div>This is the selection page!</div>;
 };
